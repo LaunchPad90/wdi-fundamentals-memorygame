3906a84b39cca00f8f1359f96aa27f5965190ead
Jacob Hickey <HIckey@Jacobs-MacBook-Air.local>
2019-12-31 22:27:19 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,28 @@
-console.log("Up and running!");
+//create arrays for cards and cardsInPlay
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [] ;
+//create variables for each card
+const cardOne = cards[3];
+const cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-let cardOne = "queen";
 
-let cardTwo = "king";
 
-let cardThree = "queen";
+//if statement to check cardsInPlay
+if (cardsInPlay.length === 2) {
+  alert('Two cards');
+} if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert('You found a match!');
+} else {
+  alert('Sorry, try again');
+}
 
-let cardFour = "king";
 
+
+
+
+
+console.log(cardsInPlay);
 console.log("User flipped " + cardOne);
-
 console.log("User flipped " + cardTwo);
