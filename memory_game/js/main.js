3290eb85c14eba1b73e50e0cd94ec79d50e7cfc9
Jacob Hickey <HIckey@Jacobs-MacBook-Air.local>
2020-01-13 07:21:24 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 //create arrays for cards and cardsInPlay
-const cards = ['queen', 'queen', 'king', 'king'];
+const cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+    {
+    rank: "queen", 
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+    {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "imgages/king-of-diamonds.png"
+  }
+];
 const cardsInPlay = []
 
 //function to check how many cardsInPlay
@@ -11,17 +32,19 @@ function flipCard(cardId) {
       } else {
     return;
   }
-  console.log('User flipped ' + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log('User flipped ' + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 
 
 //function to check if cardsInPlay match
     function checkForMatch() {
       if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log('You found a match!');
+    alert('You found a match!');
     } else {
-    console.log('Sorry, try again');
+    alert('Sorry, try again');
     }
   }
 }
